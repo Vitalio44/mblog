7606d2e14148ee3d9261faafa6d75d4112096ae2
Vitalio44 <kuzvitalii@gmail.com>
2016-10-25 22:29:55 +0300

    edit post form

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .form import PostForm
@@ -16,8 +16,8 @@ def post_create(request):
 	form = PostForm(request.POST or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		print form.cleaned_data.get("title")
 		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
 		"form": form,
 	}
@@ -33,8 +33,19 @@ def post_detail(request, id=None):
 
 
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		return HttpResponseRedirect(instance.get_absolute_url())
+	context = {
+		"instance": instance,
+	    "title": instance.title,
+	    "form": form,
+	    }
+	return render(request, "post_form.html", context)
 
 def post_delete(request):
 	return HttpResponse("<h1>Delete</h1>")
