592dedd6e6a3159e025eefcaa3960623062a2548
Vitalio44 <kuzvitalii@gmail.com>
2016-11-17 23:07:53 +0300

    slug to url

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,23 @@
 ### -*- coding: utf-8 -*- ###
 from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
+from django.db.models.signals import pre_save
 from django.db import models
 
+
 class Post(models.Model):
-    title = models.CharField(max_length=120, verbose_name = 'Заголовок')
-    image = models.ImageField(null=True, blank=True, verbose_name = 'Изображение')
-    content = models.TextField(verbose_name = 'Контент')
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name = 'Обновлено')
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name = 'Создано')
+    title = models.CharField(max_length=120, verbose_name='Заголовок')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+    content = models.TextField(verbose_name='Контент')
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Обновлено')
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Создано')
+
+    class Meta:
+        verbose_name = 'Статьи'
+        verbose_name_plural = 'Статьи'
+        ordering = ["-timestamp", "-updated"]
 
     def __unicode__(self):
         return self.title
@@ -16,12 +25,16 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolut_url(self):
-    	return reverse("detail", kwargs={"id": self.id})
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"slug": self.slug})
 
-    class Meta:
-        verbose_name = 'Статьи'
-        verbose_name_plural = 'Статьи'
-        ordering = ["-timestamp", "-updated"]
-            
-    
+
+def pre_save_post(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    exists = Post.objects.filter(slug=slug).exists()
+    if exists:
+        slug = "%s-%s" % (slug, instance.id)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_post, sender=Post)
--- posts/views.py
+++ posts/views.py
@@ -6,64 +6,66 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .form import PostForm
 from .models import Post
 
+
 def post_list(request):
-	queryset_list = Post.objects.all()
-	paginator = Paginator(queryset_list, 5) # Show 5 contacts per page
-	page = request.GET.get('page')
-	try:
-		queryset = paginator.page(page)
-	except PageNotAnInteger:
-		# If page is not an integer, deliver first page.
-		queryset = paginator.page(1)
-	except EmptyPage:
-		# If page is out of range (e.g. 9999), deliver last page of results.
-		queryset = paginator.page(paginator.num_pages)
-	context = {
-		"object_list": queryset,
-	    "title": "List"
-	    }
-	return render(request, "post_list.html", context)
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5)  # Show 5 contacts per page
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, "post_list.html", context)
 
 
 def post_create(request):
-	form = PostForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Created")
-		return HttpResponseRedirect(instance.get_absolute_url())		
-	context = {
-		"form": form,
-	}
-	return render(request, "post_form.html", context)
-
-def post_detail(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	context = {
-		"instance": instance,
-	    "title": instance.title
-	    }
-	return render(request, "post_detail.html", context)
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    context = {
+        "instance": instance,
+        "title": instance.title
+    }
+    return render(request, "post_detail.html", context)
 
-def post_update(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.save()
-		messages.success(request, "Updateted!")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	context = {
-		"instance": instance,
-	    "title": instance.title,
-	    "form": form,
-	    }
-	return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-	instance = get_object_or_404(Post, id=id)
-	instance.delete()
-	messages.success(request, "Deleted!")
-	return redirect("post:list")
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Updated!")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "instance": instance,
+        "title": instance.title,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    instance.delete()
+    messages.success(request, "Deleted!")
+    return redirect(post_list)
